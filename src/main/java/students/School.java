2fa0e456bbd35f060e3906a39534b7604e20018d
Nitin Vericherla <nitin_vericherla@intuit.com>
2021-07-14 13:35:00 -0400

    studentt and school initial code

--- src/main/java/students/School.java
+++ src/main/java/students/School.java
@@ -1,9 +1,30 @@
 package students;
 
+import java.util.Arrays;
+
 public class School {
   /*
   create several students...
   - print all the students
   - print all the "smart" students
    */
+
+  private Student[] students = {
+    new Student("Frodo", 3.4, new String[] {"Geography", "History"}),
+    new Student("Bilbo", 1.5, new String[] {"English", "History"})
+  };
+
+  public School(Student[] students) {
+    this.students = students;
+  }
+
+  private void printStudents() {
+    Arrays.stream(this.students).forEach(s -> System.out.println(s.getName()));
+  }
+
+  private void printSmartStudents() {
+    Arrays.stream(this.students)
+        .filter(s -> s.getGPA() > 3)
+        .forEach(s -> System.out.println(s.getName()));
+  }
 }
--- src/main/java/students/Student.java
+++ src/main/java/students/Student.java
@@ -5,4 +5,25 @@ public class Student {
   represent a student with name, grade (double gpa),
   courseS currently being taken : STRING!!!
    */
+  private String name;
+  private Double gpa;
+  private String[] courses;
+
+  public Student(String name, Double gpa, String[] courses) {
+    this.name = name;
+    this.gpa = gpa;
+    this.courses = courses;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Double getGPA() {
+    return this.gpa;
+  }
+
+  public String[] getCourses() {
+    return this.courses;
+  }
 }
